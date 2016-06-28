0648a83cb7ae5741c1e3f46168a6e79bb8ff2b96
Michael Gustus <michael.gustus@hp.com>
2016-06-28 15:03:57 +0300

    Create appointments in current calendar

--- QuickLook/OutlookUtils.cs
+++ QuickLook/OutlookUtils.cs
@@ -59,7 +59,7 @@ namespace QuickLook
 
     public static AppointmentItem AddAppointment(String subject, DateTime startDate, DateTime endDate, String categories, int reminderMinutesBeforeStart, Dictionary<String, Object> customFields, bool save)
     {
-      AppointmentItem newAppointment = (AppointmentItem)GetApplication().CreateItem(OlItemType.olAppointmentItem);
+      AppointmentItem newAppointment = (AppointmentItem)GetApplication().ActiveExplorer().CurrentFolder.Items.Add(OlItemType.olAppointmentItem);
       newAppointment.AllDayEvent = true;
       newAppointment.Start = startDate;
       newAppointment.End = endDate;
